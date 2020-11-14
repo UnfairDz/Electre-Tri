f1daf92fe522d09a430d3974ec3109adf8beeb6b
Ramzi Brikci-sid <brikci.sid.ramzi@gmail.com>
2020-11-14 14:16:01 +0100

    Updates

---
+++ precache-manifest.134b0837a83bc11d9b4f876efc4bf8b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f6701b12069c958086dce0df4da5439",
+    "url": "/Electre-Tri/index.html"
+  },
+  {
+    "revision": "d40300c9214bd8d11ce6",
+    "url": "/Electre-Tri/static/css/main.e1f85589.chunk.css"
+  },
+  {
+    "revision": "352bc556f3a2cbb1cd1c",
+    "url": "/Electre-Tri/static/js/2.c3171495.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Electre-Tri/static/js/2.c3171495.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d40300c9214bd8d11ce6",
+    "url": "/Electre-Tri/static/js/main.8ad77571.chunk.js"
+  },
+  {
+    "revision": "96cbceca60c6d83cb315",
+    "url": "/Electre-Tri/static/js/runtime-main.52a350ac.js"
+  }
+]);
